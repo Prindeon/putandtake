3bd8991b415b2a2fe7121e056ca3475968420113
Prindeon <marius.k@hotmail.dk>
2024-05-20 18:20:07 +0200

    0.12
    bunch of shit

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 
-//side-navbar menu opening and closing when clicking hamburger menu
+//-------------------------------------------------------------------|
+// side-navbar menu opening and closing when clicking hamburger menu |
+//-------------------------------------------------------------------|
 const hamburgerMenu = document.getElementById('hamburger-menu')
 const mobileNav = document.getElementById('mobile-nav')
 const overlay = document.getElementById('overlay')
@@ -9,23 +11,36 @@ hamburgerMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('show')
     overlay.classList.toggle('show')
 })
-
 overlay.addEventListener('click', () => {
     mobileNav.classList.remove('show')
     overlay.classList.remove('show')
 })
-
 closeBtn.addEventListener('click', () => {
     mobileNav.classList.remove('show')
     overlay.classList.remove('show')
 })
-
+//------------------------------------------------------------------|
+// dropdown opening, arrow moving, and language disappearing behind |
+//------------------------------------------------------------------|
 const dropdownBtn = document.getElementById('dropdown-btn');
 const dropdownContent = document.querySelector('.dropdown-content');
 const dropdownArrow = document.getElementById('dropdown-arrow')
+const languageSelector = document.querySelector('.language-selector')
 
 dropdownBtn.addEventListener('click', () => {
     dropdownContent.classList.toggle('show')
     dropdownArrow.classList.toggle('flipped')
+    languageSelector.classList.toggle('show')
+})
 
+
+//----------------------------------------------------------------------------------------------------|
+// makes all logo click events lead to index page - this would have to be changed for other languages |
+//----------------------------------------------------------------------------------------------------|
+const brandLogo = document.querySelectorAll('.logo')
+
+brandLogo.forEach(logo => {
+    logo.addEventListener('click', () => {
+        window.location.href = 'index.html'
+    })
 })
